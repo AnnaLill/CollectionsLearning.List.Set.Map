c08600ea020ebdee86ba984944d728a5957a9764
future <tebenkova100@icloud.com>
2023-01-15 01:03:54 +0800

    Cats example

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
+import ru.synergy.Cat;
+
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        Cat[] cats = new Cat[4];
+        cats[0] = new Cat("Томас");
+        cats[1] = new Cat("Бегемот");
+        cats[2] = new Cat("Филлипп Мапкович");
+        cats[3] = new Cat("Котяра");
+
+        cats[1] = null;
+
+        System.out.println(Arrays.toString(cats));
     }
+
 }
